69a78b8467e33bf3f45e76bcce41caebd53329a4
bozomj <bozomj@gmail.com>
2025-04-27 16:42:06 -0300

    refactor `card`

---
+++ components/card.tsx
@@ -0,0 +1,13 @@
+interface CardProps {
+  children?: React.ReactNode;
+}
+
+const Card: React.FC<CardProps> = ({ children }) => {
+  return (
+    <div className="min-h-[8rem] gap-2 mi-w-[8rem] max-w-[12rem] flex flex-col bg-cyan-950 p-2 rounded-lg justify-center items-center">
+      {children}
+    </div>
+  );
+};
+
+export default Card;
