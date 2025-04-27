6f3ac74961c5c2f180155d928c99d58162a81114
bozomj <bozomj@gmail.com>
2025-04-27 16:25:15 -0300

    add router `api/v1/posts`

---
+++ pages/api/v1/posts/index.ts
@@ -0,0 +1,32 @@
+import Post from "@/models/post";
+import { NextApiRequest, NextApiResponse } from "next";
+import { createRouter } from "next-connect";
+
+const router = createRouter<NextApiRequest, NextApiResponse>();
+
+router.get(getHandler);
+router.post(postHandler);
+
+export default router.handler();
+
+async function getHandler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const posts = await Post.listAllPost();
+
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar postagens", cause: error });
+  }
+}
+
+async function postHandler(req: NextApiRequest, res: NextApiResponse) {
+  const body = req.body;
+  console.log("corpo>>>", body);
+
+  try {
+    const post = await Post.create(body);
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(500).json({ message: "erro ao inserir post", cause: error });
+  }
+}
