c706c1325015325e7a03dfe169224f76e18461c8
bozomj <bozomj@gmail.com>
2025-04-27 14:15:35 -0300

    refactor: `ListTile` add link

--- components/ListTile.tsx
+++ components/ListTile.tsx
@@ -3,15 +3,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const ListTile = ({
   title,
   icon,
+  url,
   onClick,
 }: {
   title: string;
   icon: any;
+  url?: string;
   onClick: () => void;
 }) => {
   return (
     <div
-      className="flex items-center gap-2 p-3 cursor-pointer"
+      className="flex items-center gap-2 p-3 cursor-pointer hover:text-cyan-500"
       onClick={onClick}
     >
       <span>
@@ -21,7 +23,7 @@ const ListTile = ({
         tabIndex={0}
         className="opacity-0 group-focus:block whitespace-nowrap group-focus:opacity-100 transition-all duration-500 group-hover:opacity-100"
       >
-        {title}
+        {url ? <a href={url || ""}>{title}</a> : title}
       </span>
     </div>
   );
