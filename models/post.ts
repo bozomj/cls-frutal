14b88efb619436a4170accd2089f03ab4497129d
bozomj <bozomj@gmail.com>
2025-05-27 21:04:48 -0300

    create and use  function in models/post.create

--- models/post.ts
+++ models/post.ts
@@ -5,11 +5,38 @@ export type postType = {
   userId: string;
   title: string;
   description: string;
+  valor: number;
+  categoria_id: string;
   content: string;
   createdAt?: EpochTimeStamp;
 };
 
+function isPostType(obj: unknown): obj is postType {
+  if (typeof obj !== "object" || obj === null) return false;
+  const o = obj as Record<string, unknown>;
+  return (
+    typeof o.userId === "string" &&
+    typeof o.title === "string" &&
+    typeof o.description === "string" &&
+    typeof o.valor === "number" &&
+    typeof o.categoria_id === "string" &&
+    typeof o.content === "string"
+  );
+}
+
 async function create(pst: postType) {
+  if (!isPostType(pst)) {
+    throw {
+      message: "JSON incorreto",
+    };
+  }
+
+  Object.values(pst).forEach((v) => {
+    if (v == "" || v == null) {
+      throw "campo com valores incorretos";
+    }
+  });
+
   const query =
     'INSERT INTO posts ("userId", title, description, content) VALUES ($1,$2,$3,$4) RETURNING *;';
 
