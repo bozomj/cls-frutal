9f69de8aeaabd6d0d2ea0b10eff5989980315813
bozomj <bozomj@gmail.com>
2025-07-28 18:16:47 -0300

    add metodo put in /api/v1/posts

--- pages/api/v1/posts/index.ts
+++ pages/api/v1/posts/index.ts
@@ -7,30 +7,21 @@ const router = createRouter<NextApiRequest, NextApiResponse>();
 
 router.get(getHandler);
 router.post(postHandler);
+router.put(putHandler);
 
 export default router.handler();
 
 async function getHandler(req: NextApiRequest, res: NextApiResponse) {
-  const search = req.query.search as string;
+  const search = (req.query.search as string) || "";
   const initial = (req.query.initial as string) || "0";
-  const limit = (req.query.limit as string) || "0";
-
-  if (search) {
-    try {
-      const posts = await Post.search(search || "", initial, limit);
-
-      res.status(200).json(posts);
-    } catch (error) {
-      throw { message: "erro com a pesquisa", cause: error };
-    }
-  }
+  const limit = req.query.limit as string | null;
 
   try {
-    const posts = await Post.listAllPost(initial, limit);
+    const posts = await Post.search(search, initial, limit);
 
     res.status(200).json(posts);
   } catch (error) {
-    res.status(500).json({ error: "Erro ao buscar postagens", cause: error });
+    throw { message: "Erro ao buscar postagens", cause: error };
   }
 }
 
@@ -45,6 +36,35 @@ async function postHandler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const post = await Post.create(body);
 
+    return res.status(201).json(post);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "erro ao inserir post", cause: error });
+  }
+}
+
+async function putHandler(req: NextApiRequest, res: NextApiResponse) {
+  const body = req.body;
+  let user;
+
+  try {
+    user = autenticator.verifyToken(req.cookies.token || "");
+
+    if (user.id !== body.user_id) {
+      throw {
+        message: "Unauthorized",
+        cause: "Post não pertence ao usuario atual",
+      };
+    } else {
+      body.user_id = user.id;
+    }
+  } catch (e) {
+    return res.status(401).json({ message: "Unauthorized", cause: e });
+  }
+
+  try {
+    const post = await Post.update(body);
     return res.status(201).json(post);
   } catch (error) {
     return res
