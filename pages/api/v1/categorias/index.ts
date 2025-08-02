43df0911a8a7255c5001fac1fed7e9be5e6dd5f4
bozomj <bozomj@gmail.com>
2025-08-02 17:53:14 -0300

    add postHandler in api/v1/categorias

--- pages/api/v1/categorias/index.ts
+++ pages/api/v1/categorias/index.ts
@@ -1,3 +1,4 @@
+import Categoria from "@/models/categoria";
 import categoria from "@/models/categoria";
 import { NextApiRequest, NextApiResponse } from "next";
 import { createRouter } from "next-connect";
@@ -5,6 +6,7 @@ import { createRouter } from "next-connect";
 const router = createRouter<NextApiRequest, NextApiResponse>();
 
 router.get(getHandler);
+router.post(postHandler);
 
 export default router.handler();
 
@@ -12,4 +14,17 @@ async function getHandler(req: NextApiRequest, res: NextApiResponse) {
   const result = await categoria.getAll();
 
   res.status(200).json(result);
+}
+
+async function postHandler(req: NextApiRequest, res: NextApiResponse) {
+  const categ = req.body;
+
+  try {
+    const result = await Categoria.save(categ.descricao);
+    return res.status(201).json(result);
+  } catch (error) {
+    return res.status(405).json(error);
+  }
+
+  // const result = await categoria.save()
 }
