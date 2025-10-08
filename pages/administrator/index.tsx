bf68c00087c4731d8622e4b26639695410f3e3a4
bozomj <bozomj@gmail.com>
2025-10-07 22:42:08 -0300

    x

--- pages/administrator/index.tsx
+++ pages/administrator/index.tsx
@@ -1,19 +1,50 @@
+import CircleAvatar from "@/components/CircleAvatar";
 import autenticator from "@/models/autenticator";
 import User, { UserType } from "@/models/user";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { useEffect, useState } from "react";
 
 interface Props {
   user: UserType;
 }
 
+async function getUsers() {
+  const result = await fetch("http://localhost:3000/api/v1/users", {});
+  const users = await result.json();
+
+  return users;
+}
+
+function mountUserList(users: UserType[]) {
+  console.log(users);
+  return users.map((user) => (
+    <div
+      key={user.id}
+      className="p-2 border-b bg-cyan-600 flex gap-4 items-center "
+    >
+      <div>{user.name}</div>
+      <div>{user.email}</div>
+    </div>
+  ));
+}
+
 const AdministratorPage = ({ user }: Props) => {
-  console.log(user);
+  const [users, setUsers] = useState<UserType[]>([]);
+
+  useEffect(() => {
+    getUsers().then(setUsers);
+  }, []);
 
   return (
-    <>
+    <div className="p-2">
       <div>Administrator Page</div>
       <div>{user.id}</div>
-    </>
+      <CircleAvatar imagem={user.url} size={8} />
+      <section className="flex gap-1 flex-col">
+        <h2 className="text-2xl font-bold my-4 ">Lista de usuarios</h2>
+        {mountUserList(users)}
+      </section>
+    </div>
   );
 };
 
