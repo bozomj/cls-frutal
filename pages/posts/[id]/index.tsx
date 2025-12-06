d91b1caaddf726ef7dba7ba0fa5a949fe3ce7447
bozomj <bozomj@gmail.com>
2025-12-05 22:45:02 -0300

    x

--- pages/posts/[id]/index.tsx
+++ pages/posts/[id]/index.tsx
@@ -419,13 +419,19 @@ export default function DetailsPostPage({ user_id }: Props) {
                                   }
                                 );
 
-                                await fetch("/api/v1/uploadImages", {
-                                  method: "POST",
-                                  headers: {
-                                    "Content-Type": "application/json",
-                                  },
-                                  body: JSON.stringify(imgs),
-                                });
+                                await fetch(
+                                  `${
+                                    process.env.URLDOMAIN ||
+                                    "http://localhost:300"
+                                  }/api/v1/uploadImages`,
+                                  {
+                                    method: "POST",
+                                    headers: {
+                                      "Content-Type": "application/json",
+                                    },
+                                    body: JSON.stringify(imgs),
+                                  }
+                                );
 
                                 setPreviewImagens([]);
                                 router.replace(router.asPath);
