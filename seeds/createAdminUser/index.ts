aa2e27e43fe3a7a96d48bec52e634df62549157a
bozomj <bozomj@gmail.com>
2026-08-24 21:36:02 -0300

    change seed createAdminUser add `create:session` in features

--- seeds/createAdminUser/index.ts
+++ seeds/createAdminUser/index.ts
@@ -11,11 +11,29 @@ export default async function createAminUser() {
   };
 
   try {
-    await User.create(user);
+    const us = await User.create(user);
+    await User.setFeatures(us[0].id, ["create:session"]);
     console.log("✅ Usuário Administrador criado com sucesso!");
   } catch (error) {
+    console.log(">>>>>| ", error);
     return error;
   }
 }
+async function run() {
+  console.log("🚀 Iniciando seed de AdminUser...");
+  await createAminUser();
+  console.log("🏁 Processo de seed finalizado.");
+  process.exit(0); // Fecha o processo do Node com sucesso
+}
 
 // 2. O PULO DO GATO: Função autoexecutável para quando o script rodar via terminal
+if (
+  require.main === module ||
+  (process.argv[1] && process.argv[1].includes("createAdminUser"))
+) {
+  console.log("🚀 Iniciando seed de Administrador via terminal...");
+  run().catch((err) => {
+    console.error("❌ Erro fatal ao executar o seed de categorias:", err);
+    process.exit(1); // Fecha o processo avisando que deu erro
+  });
+}
